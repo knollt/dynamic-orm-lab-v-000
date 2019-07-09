75e518c0d09592a682f5bbf018abdd5d9854aaf3
Tim Knoll <timothy.knoll@gmail.com>
2019-07-09 03:00:33 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -39,9 +39,13 @@ class InteractiveRecord
 
   # save - INSERT INTO students (name, grade) VALUES (x,y)
   def save
+    # binding.pry
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    # binding.pry
     DB[:conn].execute(sql)
+    # binding.pry
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    # binding.pry
   end
   
   def table_name_for_insert 
